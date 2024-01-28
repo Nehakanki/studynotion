15a3d4915d39d2fbe5045dd57cb62ed1f9cb1b65
Neha <kankineha@gmail.com>
2024-01-28 20:53:41 +0530

    Navbar : left side button

--- frontend/src/components/core/Navbar.jsx
+++ frontend/src/components/core/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link, useLocation, matchPath } from 'react-router-dom';
 import logo from '../../assests/logos/Studynotionlogo.svg';
 import { NavbarLinks } from '../../components/core/HomePag/data/NavbarLink';
 import {  useSelector } from 'react-redux';
+import { IoCart, IoNutrition } from "react-icons/io5";
+import ProfileDropdown from './Auth/ProfileDropdown';
 
 const Navbar = () => {
 
@@ -16,8 +18,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
-      <div className='flex flex-row w-11/12 max-w-maxContent items-center justify-between'>
+    <div className='flex h-14 items-center justify-evenly border-b-[1px] border-b-richblack-700'>
+      <div className='flex flex-row w-11/12 max-w-maxContent items-center justify-evenly'>
         {/* Logo Added */}
         <Link to='/'>
           <img src={logo} alt="Main logo" />
@@ -40,13 +42,53 @@ const Navbar = () => {
           </ul>
         </nav>
       </div>
-
+     
+      
       {/* login/ SIgnup/Dashboard */}
+
+      {/*  1 . if the user is Student the show the cart and no. of courses bought by him */}
       <div className='flex gap-x-4 items-center'>
+      
         {
-          ''
+          user && user?.accountType != 'Instructor' && (
+            <Link to='/dashboard/cart' className='relative' >
+                <IoCart />
+                {/* placing number above the cart */}
+                {
+                  totalItems >0 && (
+                    <span>
+                      {totalItems}
+                    </span>
+                  )
+                }
+            </Link>
+          )
         }
-        </div>
+        {/* signup and login button*/}
+        {
+          token === null && (
+            <Link to ="/login">
+              <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-25 rounded-md'>
+                Login
+              </button>
+            </Link>
+          )
+        }
+        {/*  */}
+        {
+            token === null && (
+              <Link to="/signup">
+                <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-50 rounded-md' >
+                  Sign up
+                </button>
+              </Link>
+            )
+        }
+        {/* login user */}
+        {
+          token !=null &&  <ProfileDropdown/>
+        }
+      </div>
 
     </div>
   );
