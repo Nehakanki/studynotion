422c131815c624fa15f4fb6aba9b57db05ce305c
Neha <kankineha@gmail.com>
2024-01-31 22:46:13 +0530

    logo page image into assets

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logoImage from '../assests/banner/login/Frame 22.jpg'
+
+
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
