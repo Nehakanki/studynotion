f223765e4ad6d7dc6c1855029796ccdbc30221d6
Neha <kankineha@gmail.com>
2024-02-03 21:27:35 +0530

    Auth slice changes

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -6,6 +6,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
+    signupData :null,
+    loading: false,
     token : localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
 };
 
@@ -13,11 +15,17 @@ const authSlice = createSlice({
     name:"auth",
     initialState:initialState,
     reducers:{
+        setSignupData(state,value){
+            state.signupData = value.payload;
+        },
+        setLoading(state,value){
+            state.loading = value.payload;
+        },
         setToken(state, value){
             state.token = value.payload
         },
     },
 });
 
-export const {setToken} = authSlice.actions
+export const {setToken, setLoading, setSignupData} = authSlice.actions
 export default authSlice.reducer;
