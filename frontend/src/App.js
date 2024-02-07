00b50d9edd542e41b3d9caafb71cb1d2aa0c683c
Neha <kankineha@gmail.com>
2024-02-07 23:49:31 +0530

    Forgot Password page started -1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,28 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home'; 
 import Login from './Pages/Login';
 import Navbar from './components/core/Navbar'
+import Signup from './Pages/Signup';
 
 const App = ()=> {
   return (
     <div className='w-screen min-h-screen flex flex-col font-inter bg-richblack-800 '>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={
+          <OpenRoute> <Home/></OpenRoute>
+       
+        
+        }/>
+        <Route path='/login' element={
+
+
+<OpenRoute> <Login/></OpenRoute>
+        }/>
+        <Route path= '/signup' element={
+        
+        
+        <OpenRoute> <Signup/></OpenRoute>
+        }/>
 
        </Routes>
 
--- frontend/src/Pages/ForgotPassword.jsx
+++ frontend/src/Pages/ForgotPassword.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { UseSelector, useDispatch } from 'react-redux'
+import {Link} from 'react-router-dom'
 
 const ForgotPassword = () => {
 
   // 2 page are to be shown 
-  // 1st one: entertong Email page
-  // 2nd one: Resend the Email
+ 
+  // 1st one: Resend the Email
+   // 2nd one: Check Your Email
 
     const [email, setEmail] = useState("")
     const [emailSent, setEmailSent] = useState(false)
-    const dispatch = useDispatch()
+    const dispatch = dispatch()
     const { loading } = useSelector((state) => state.auth)
   return (
-    <div>ForgotPassword</div>
+    <div>
+      {
+        loading ? <div>Loading ....</div>:
+        // show content 
+          <div>
+              {/* if there is EmailSent then page 2 else page 1 */}
+             <h1>
+              {
+                !emailSent ? "Reset your password" :"Check Your Email"
+              }
+             </h1>
+
+             <p>
+                {
+                  !emailSent ?"Have no fear. We’ll email you instructions to reset your passwid. If you dont have access to your email we can try account recovery"  :`We have sent the reset email to ${email}
+                  `
+                }
+             </p>
+
+             <form>
+                {
+                     !emailSent && (
+                      <label>
+                        <p>Email Address *</p>
+                        <input
+                         required
+                         type ="email"
+                         name ="email"
+                         value ={email}
+                         onChange={(e)=> setEmail(e.target.value)}
+                         placeholder='Enter your Email Address'
+      
+                        />
+                      </label>
+                   
+                     )
+                }
+
+                <button>
+                  {
+                    emailSent ? "Reset password":"Check your Email"
+                  }
+                </button>
+             </form>
+
+             <div>
+              <Link to="/login">
+                <p>Back to login</p>
+              </Link>
+              </div>
+             
+          </div>
+      }
+      
+      
+      
+    </div>
+
     
   )
 }
