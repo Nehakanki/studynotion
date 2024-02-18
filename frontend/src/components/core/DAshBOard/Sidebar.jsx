658cdb9c3cd199bd8558d59ca2300decf53a00ec
Neha <kankineha@gmail.com>
2024-02-18 17:02:43 +0530

    Logout Confirmation Modal

--- frontend/src/components/core/DAshBOard/Sidebar.jsx
+++ frontend/src/components/core/DAshBOard/Sidebar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { sidebarLinks } from '../HomePag/data/dashboardLinks';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { VscSignOut } from "react-icons/vsc"
+import { logout } from '../../../services/operations/authApi';
 import SidebarLinks from './SidebarLinks';
+import ConfirmationModal from './ConfirmationModal';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
   const {loading:authLoading} = useSelector((state)=>state.auth);
   const {loading:profileLoading} = useSelector((state)=>state.profile);
+
+// for showing the Logout Confirmation Modal
+  const [ConfirmationModal, setConfirmationModal] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // show loading if any of the 2 loading is true
   if(authLoading || profileLoading){
     <div className='flex h-[calc(100vh - 3.5rem)]  min-w-[220px] items-center justify-center border-r-[1px] border-r-richblack-700 bg-richblack-800' >
        <div className="spinner"></div>
@@ -13,11 +23,14 @@ const Sidebar = () => {
   }
   return (
     <div>
+     
       <div className="flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10">
         <div className="flex flex-col">
+          {/* for each link */}
           {sidebarLinks.map((link)=>{
             if(link.type && user?.accountType !== link.type ) return null;
-            <SidebarLinks/>
+           
+            <SidebarLinks link={link} key={link.id} iconName={link.icon} />
 
           })
           
@@ -26,8 +39,57 @@ const Sidebar = () => {
 
 
         </div>
+
+        {/* Setting and Logout Button */}
+        <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700">
+       
+       
+          <SidebarLinks  link={{ name: "Settings", path: "/dashboard/settings" }} 
+            iconName="VscSettingsGear"
+            
+          />
+
+          {/* button for Logout and onCLick a Modal is shown */}
+
+          <button
+          onClick={setConfirmationModal({
+            txt1: "Are You Sure ?",
+            txt2: "You will be logged Out from your Account",
+            btn1Text: "Logout",
+            btn2Text: "Cancel",
+            btn1Handler:()=>dispatch(logout(navigate)),
+            
+            btn2Handler:()=>setConfirmationModal(null)
+          })}
+         className="text-sm font-medium text-richblack-300 px-6 py-2 "
+
+          
+          
+          
+          >
+{/* CSS make sure */}
+        <div className="flex items-center justify-center gap-x-2">
+              <VscSignOut className="text-lg" />
+              <span>Logout</span>
+            </div>
+
+
+
+          </button>
+
+
+
+
+
+
+        </div>
+
+
       </div>
 
+          {
+            ConfirmationModal && <ConfirmationModal setdata={setConfirmationModal} />
+          }
 
     </div>
   )
--- frontend/src/components/core/DAshBOard/SidebarLinks.jsx
+++ frontend/src/components/core/DAshBOard/SidebarLinks.jsx
@@ -38,6 +38,7 @@ const SidebarLinks = ({link, iconName}) => {
             </div>
 
 
+            
 
         </NavLink>
 
